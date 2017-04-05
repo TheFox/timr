6429e789ea944948f21aba7b4796d7560e9c21be
Christian Mayer <christian@fox21.at>
2017-04-05 12:21:53 +0200

    Config basic tests added. #4

---
+++ test/test_config.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require 'timr'
+
+class TestConfig < MiniTest::Test
+	
+	# include TheFox::Timr
+	include TheFox::Timr::Model
+	# include TheFox::Timr::Error
+	
+	def test_config
+		config1 = Config.new
+	end
+	
+end
