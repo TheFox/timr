d922d6b383526c3e531aa38fb53346f207b4f4ca
Christian Mayer <christian@fox21.at>
2017-04-14 08:16:18 +0200

    Do not ignore man/index.
    Needed for generating man pages.

--- lib/timr/version.rb
+++ lib/timr/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Timr
 		NAME = 'Timr'
-		VERSION = '0.4.0'
-		DATE = '2017-04-13'
+		VERSION = '0.5.0-dev.1'
+		DATE = '2017-04-14'
 		HOMEPAGE = 'https://github.com/TheFox/timr'
 		
 		MODEL_DATETIME_FORMAT = '%FT%T%z'
